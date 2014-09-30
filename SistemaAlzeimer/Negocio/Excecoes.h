9a3b8fd4e632d23c34c048d1c1e28263be6b40a9
hhaliski <haliski.henrique@gmail.com>
2014-09-30 16:48:15 -0300

    Update Excecoes.h

--- SistemaAlzeimer/Negocio/Excecoes.h
+++ SistemaAlzeimer/Negocio/Excecoes.h
@@ -1,8 +1,20 @@
 #ifndef EXCECOES_H
 #define EXCECOES_H
+#include "ListaPaciente.h"
+#include "ListaTratamento.h"
+#include "Paciente.h"
+#include "Arquivo.h"
 
 
+class Excecoes{
+public:
+    void ExcecoesCadastrarPaciente(ListaPaciente *lista);
+    void ExcecoesAlterarPaciente(ListaPaciente *lista);
+    void ExcecoesExcluirPaciente(ListaPaciente *lista);
+    void ExcecoesConsultarPaciente(ListaPaciente *lista);
+    ListaPaciente *lista= new ListaPaciente;
+    Arquivo arquivo;
 
-
+};
 
 #endif // EXCECOES_H
